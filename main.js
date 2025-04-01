04e8b026a2117a95283387ac15dcb92282102b6b
anaaragon88 <ana.aragon88@gmail.com>
2025-04-01 11:49:47 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+document.addEventListener(
+  "DOMContentLoaded",
+
+  function () {
+    const boton = document.querySelector(".button-toggle");
+    const menuLinks = document.querySelector(".menu");
+
+    boton.addEventListener("click", function () {
+      menuLinks.classList.toggle("open");
+    });
+  }
+);
